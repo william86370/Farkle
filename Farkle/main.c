c67ac1ca6f1b0cecc4411410ff6352d1eb393c54
William Wright <william86370@gmail.com>
2017-11-16 20:28:42 -0500

    Initial Commit

---
+++ Farkle/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Farkle
+//
+//  Created by william wright on 11/16/17.
+//  Copyright © 2017 william wright. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
